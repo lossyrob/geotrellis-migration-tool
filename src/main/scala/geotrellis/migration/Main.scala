dd9d9c5cb74cf2914a3ede0b77670377b83f852c
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-04-11 15:29:56 +0300

    App -> to main method

--- src/main/scala/geotrellis/migration/Main.scala
+++ src/main/scala/geotrellis/migration/Main.scala
@@ -3,18 +3,20 @@ package geotrellis.migration
 import geotrellis.migration.cli.MainOptions
 import geotrellis.migration.core.backend._
 
-object Main extends App {
-  MainOptions.parse(args) match {
-    case Some(config) => {
-      config.transformArgs.format match {
-        case "accumulo" => AccumuloTools(config.accumuloArgs)
-        case "file"     => FileTools(config.fileArgs)
-        case "hadoop"   => HadoopTools(config.hadoopArgs)
-        case "s3"       => S3Tools(config.s3Args)
-        case ""         => throw new Exception("No backend specified")
-        case _          => throw new Exception("Wrong backend specified")
+object Main {
+  def main(args: Array[String]): Unit = {
+    MainOptions.parse(args) match {
+      case Some(config) => {
+        config.transformArgs.format match {
+          case "accumulo" => AccumuloTools(config.accumuloArgs)
+          case "file" => FileTools(config.fileArgs)
+          case "hadoop" => HadoopTools(config.hadoopArgs)
+          case "s3" => S3Tools(config.s3Args)
+          case "" => throw new Exception("No backend specified")
+          case _ => throw new Exception("Wrong backend specified")
+        }
       }
+      case None => throw new Exception("No valid arguments passed")
     }
-    case None => throw new Exception("No valid arguments passed")
   }
 }
